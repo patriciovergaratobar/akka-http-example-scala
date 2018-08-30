072549f64162e45b517054aa5a29d79b39a4f242
patriciovergaratobar <patricio.vergara.tobar@gmail.com>
2018-08-30 01:14:40 -0300

    Fix User Route

--- src/main/scala/com/pvergara/lab/akkahttp/http/routes/UserRoute.scala
+++ src/main/scala/com/pvergara/lab/akkahttp/http/routes/UserRoute.scala
@@ -1,69 +1,72 @@
 package com.pvergara.lab.akkahttp.http.routes
 
-import akka.http.scaladsl.model.{ContentTypes, HttpEntity, HttpMethod, StatusCodes}
-import akka.http.scaladsl.server.Directive1
-import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity, StatusCodes}
+import akka.http.scaladsl.server.Directives.{put, _}
 import com.pvergara.lab.akkahttp.model.{BasicResponseModel, UserModel, UsersModel}
 import com.pvergara.lab.akkahttp.core.controller.Controller.BaseController
 import com.pvergara.lab.akkahttp.http.JsonSupport
 
-import scala.concurrent.Future
+/**
+  * Class Route WS Users Data
+  * @param controller type UserModel
+  */
+class UserRoute(implicit controller: BaseController[UserModel])
+  extends JsonSupport {
 
-
-class UserRoute(implicit controller: BaseController[UserModel]) extends JsonSupport {
-
-  val route =
-        path("user") {
-          get {
-            complete(
-              StatusCodes.OK  ->
-                HttpEntity(
-                  ContentTypes.`application/json`,
-                  usersJsonFormat.write(UsersModel(controller.getAll())).toString()
-                )
+  val route = pathPrefix("user") {
+    path("id" / Segment) { pathParam =>
+      get {
+        complete(
+          StatusCodes.OK ->
+            HttpEntity(
+              ContentTypes.`application/json`,
+              userJsonFormat.write(controller.get(pathParam.toInt)).toString()
             )
-          }
-        } ~ path("user" / Segment) {
-          pathParam =>
-            get {
-              complete(
-                StatusCodes.OK  ->
-                  HttpEntity(
-                    ContentTypes.`application/json`,
-                    userJsonFormat.write(controller.get(pathParam.toInt)).toString()
-                  )
-              )
-            }
-        } ~ path("user") {
-          post {
-            entity(as[UserModel]) { user => complete(
-                StatusCodes.OK  -> HttpEntity(
-                  ContentTypes.`application/json`,
-                  basicResponseJson.write(BasicResponseModel(controller.create(user), "")).toString()
-                )
-              )
-            }
-          }
-        } ~ path("user") {
-          put {
-            entity(as[UserModel]) { user => complete(
-              StatusCodes.OK  -> HttpEntity(
-                  ContentTypes.`application/json`,
-                  basicResponseJson.write(BasicResponseModel(controller.update(user), "")).toString()
-                )
-            )
-          }
-        } ~ path("user" / Segment) {
-            pathParam =>
-              delete {
-                complete(
-                  StatusCodes.OK  ->
-                    HttpEntity(
-                      ContentTypes.`application/json`,
-                      basicResponseJson.write(BasicResponseModel(controller.detele(pathParam.toInt), "")).toString()
-                    )
-                )
-              }
-          }
+        )
       }
+    } ~ get {
+      complete(
+        StatusCodes.OK ->
+          HttpEntity(
+            ContentTypes.`application/json`,
+            usersJsonFormat.write(UsersModel(controller.getAll())).toString()
+          )
+      )
+    } ~ post {
+      entity(as[UserModel]) { user =>
+        complete(
+          StatusCodes.OK -> HttpEntity(
+            ContentTypes.`application/json`,
+            basicResponseJson
+              .write(BasicResponseModel(controller.create(user), ""))
+              .toString()
+          )
+        )
+      }
+    } ~ put {
+      entity(as[UserModel]) { user =>
+        complete(
+          StatusCodes.OK -> HttpEntity(
+            ContentTypes.`application/json`,
+            basicResponseJson
+              .write(BasicResponseModel(controller.update(user), ""))
+              .toString()
+          )
+        )
+      }
+    } ~ path(Segment) { pathParam =>
+      delete {
+        complete(
+          StatusCodes.OK ->
+            HttpEntity(
+              ContentTypes.`application/json`,
+              basicResponseJson
+                .write(
+                  BasicResponseModel(controller.detele(pathParam.toInt), ""))
+                .toString()
+            )
+        )
+      }
+    }
+  }
 }
